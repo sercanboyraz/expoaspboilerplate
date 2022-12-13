290383ec99dc3561c929578783abf6169c21fb51
SBoyraz <sercan@datamind.com.tr>
2022-12-13 20:39:17 +0300

    update package

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Appearance } from 'react-native';
+import { StyleSheet, Text, View, Button, Appearance, Image } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Dashboard from './src/scenes/dashboard/dashboards';
@@ -11,20 +11,41 @@ const colorScheme = Appearance.getColorScheme();
 if (colorScheme === 'dark') {
   // Use dark color scheme
 }
+
+function HeaderLogo() {
+  return (
+    <View style={{ flex: 1, flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center' }}>
+      <Image
+        style={{ width: 30, height: 30 }}
+        source={require('./assets/favicon.png')}
+      />
+      <Text style={{ color: 'white', padding: 5, fontSize: 22 }}>Home</Text>
+    </View>
+  );
+}
+
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Dashboard">
-        <Drawer.Screen name="Dashboard" component={Dashboard} />
+      <Drawer.Navigator initialRouteName="Dashboard"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: 'darkblue',
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Drawer.Screen name="Dashboard" component={Dashboard} options={{ headerTitle: () => <HeaderLogo /> }} />
         <Drawer.Screen name="Users" component={Users} />
         <Drawer.Screen name="Roles" component={Roles} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
